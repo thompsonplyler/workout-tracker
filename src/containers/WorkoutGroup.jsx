2f72e57886213bf871c6a4bca5c3a2e104ff4b8f
Thompson Plyler <nealok.g@gmail.com>
2019-02-27 10:28:31 -0500

    Working on React after workout backend.

--- src/containers/WorkoutGroup.jsx
+++ src/containers/WorkoutGroup.jsx
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
 
 class WorkoutGroup extends Component {
+  state={
+    sessions:[]
+  }
 
   componentDidMount(){
-
+    fetch("http://localhost:3001/api/v1/user_sessions")
+      .then(r=>r.json())
+      .then(uSessions=>this.setState({sessions:uSessions}))
   }
 
   render(){
+    let workoutList = this.state.sessions.map(session=> {return <li>
+      {session.session.name}
+    </li>
+
+    })
+    // {sessions} = this.state
     // console.log("Sidebar props", this.props)
-    return <div className="WorkoutList">Poop is poop is poop is poop</div>
+    return <div className="WorkoutList"><ul>{workoutList}</ul></div>
     // (<div>
     
       
